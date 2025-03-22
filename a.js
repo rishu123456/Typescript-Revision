080a4c10d794331c042891814031dc3079c13e38
rishu123456 <rishu25112001@gamil.com>
2025-03-22 09:49:51 +0530

    initial

---
+++ a.js
@@ -0,0 +1,5 @@
+"use strict";
+function greet(firstname, lastname) {
+    console.log("hii " + firstname + " " + lastname);
+}
+greet("rishu", "rai");
---
+++ a.ts
@@ -0,0 +1,5 @@
+function greet(firstname:string,lastname:string){
+    console.log("hii "+firstname+" "+lastname);
+}
+
+greet("rishu","rai");
